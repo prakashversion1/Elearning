c0723dfa26ddde632d2f0c600550d18d63bdc076
prakashversion1 <prakashjoshiversion1@gmail.com>
2015-04-28 20:12:28 +0545

    Added mongodb to the application

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var favicon = require('serve-favicon');
+var mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
 function compile(str,path){
-	return stylus(str),set('filename',path);
+	return stylus(str).set('filename',path);
 }
 
 
@@ -19,8 +20,6 @@ app.set('views', path.join(__dirname, '/server/views'));
 app.set('view engine', 'jade');
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
-//server static contents from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -31,10 +30,33 @@ app.use(stylus.middleware(
       compile: compile
     }
 ));
+//server static contents from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect('mongodb://localhost/multivision');
+var db = mongoose.connection;
+db.on('error',console.error.bind(console,'connection error .........'));
+db.once('open',function callback(){
+	console.log('multivision db opened');
+});
+
+var messageSchema = mongoose.Schema({message:String});
+var Message = mongoose.model('Message',messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err,messageDoc){
+	mongoMessage = messageDoc.message;
+});
+
+app.get('/partials/:partialPath',function(req,res){
+	res.render('partials/'+ req.params.partialPath);
+});
 
 app.get('*',function(req,res){
-	res.render('index');
+	res.render('index',{
+		mongoMessage:mongoMessage
+	});
 });
+
 
 var port = 3030;
 app.listen(port);
