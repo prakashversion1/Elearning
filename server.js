2bc634d36147c189709196c40b3bfc3f98e1e7d5
prakashversion1 <prakashjoshiversion1@gmail.com>
2015-04-30 09:16:05 +0545

    Changed partial locations

--- server.js
+++ server.js
@@ -49,6 +49,9 @@ app.get('/partials/*',function(req,res){
     var partialPath = req.path
     while(partialPath.charAt(0) === '/')
         partialPath = partialPath.substr(1);
+    partialPath = '../../public/app/'+partialPath
+    partialPath = partialPath.replace('partials/','')
+    console.log('redirecting to new partials :======> ' + partialPath)
 	res.render(partialPath);
 });
 
